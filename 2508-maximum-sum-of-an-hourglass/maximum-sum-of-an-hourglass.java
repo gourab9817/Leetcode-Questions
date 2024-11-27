edf8d720b51b92ba55de2f9a214fed10b3beb920
Gourab Choudhury <129882446+gourab9817@users.noreply.github.com>
2024-11-27 23:41:45 +0530

    Time: 8 ms (33.36%) | Memory: 44.7 MB (69.24%) - LeetSync

--- 2508-maximum-sum-of-an-hourglass/maximum-sum-of-an-hourglass.java
+++ 2508-maximum-sum-of-an-hourglass/maximum-sum-of-an-hourglass.java
@@ -1,15 +1,43 @@
+import java.util.Scanner;
+
 public class Solution {
-    public static int maxSum(int arr[][]){
-        int m = arr.length;
-        int n = arr[0].length;
-        if(m<3 || n<3) return -1;
-        int summ=Integer.MIN_VALUE;
-        for(int i=0;i<m-2;i++){
-            for(int j=0;j<n-2;j++){
-                int sum2=arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
-                summ=Math.max(summ,sum2);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows and columns:");
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        
+        if (m < 3 || n < 3) {
+            System.out.println("Hourglass pattern is not possible for matrices smaller than 3x3.");
+            sc.close();
+            return;
+        }
+        
+        int[][] arr = new int[m][n];
+        System.out.println("Enter the matrix elements:");
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
             }
         }
-        return summ;
+        
+        sc.close();
+        
+        int res = maxSum(arr);
+        System.out.println("Maximum Hourglass Sum: " + res);
+    }
+
+    public static int maxSum(int[][] arr) {
+        int maxHourGlassSum = Integer.MIN_VALUE;
+        
+        for (int i = 0; i <= arr.length - 3; i++) {
+            for (int j = 0; j <= arr[0].length - 3; j++) {
+                int currentHourGlassSum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+                                        + arr[i + 1][j + 1]
+                                        + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+                maxHourGlassSum = Math.max(maxHourGlassSum, currentHourGlassSum);
+            }
+        }
+        return maxHourGlassSum;
     }
 }
