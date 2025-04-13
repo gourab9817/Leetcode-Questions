f13ce59274c670c883cca620660f55b7a26095a8
Gourab Choudhury <129882446+gourab9817@users.noreply.github.com>
2025-04-13 11:09:29 +0530

    Time: 18 ms (78.69%) | Memory: 51.7 MB (18.77%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -13,13 +13,15 @@ class Solution {
     // }
     public static int numDistinct(String s, String t){
         // tabulization4
-        int dp[][]=new int[s.length()+1][t.length()+1];
-        for(int i=0;i<=s.length();i++)
+        int m = s.length();
+        int n = t.length();
+        int dp[][]=new int[m+1][n+1];
+        for(int i=0;i<=m;i++)
         {
             dp[i][0]=1;
         }
-        for(int i=1;i<=s.length();i++){
-            for(int j=1;j<=t.length();j++){
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
                 if(s.charAt(i-1)==t.charAt(j-1)){
                     dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
                 }else{
@@ -28,6 +30,6 @@ class Solution {
                 
             }  
         }
-        return dp[s.length()][t.length()];
+        return dp[m][n];
     }
 }
