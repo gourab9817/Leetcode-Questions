b94cf5fc0bde010da6fd53d56fbe0583ccd930d1
Gourab Choudhury <129882446+gourab9817@users.noreply.github.com>
2025-04-13 11:39:08 +0530

    Time: 20 ms (43.80%) | Memory: 51.4 MB (42.77%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,35 +1,44 @@
 class Solution {
-    // public static int helper(String s, String t,int i,int j){
-    //     if(j<0){
-    //         return 1;
-    //     }
-    //     if(i<0) return 0;
-    //     if(s.charAt(i)==t.charAt(j)){
-    //        return helper(s,t,i-1,j-1)+ helper(s,t,i-1,j);
-    //     }else{
-    //         return helper(s,t,i-1,j);
+    // public static int numDistinct(String s, String t){
+    //     int m = s.length();
+    //     int n = t.length();
+    //     int dp[][]=new int[m+1][n+1];
+    //     for(int i=0;i<=m;i++)
+    //     {
+    //         dp[i][0]=1;
     //     }
         
+        
     // }
-    public static int numDistinct(String s, String t){
-        // tabulization4
-        int m = s.length();
-        int n = t.length();
-        int dp[][]=new int[m+1][n+1];
-        for(int i=0;i<=m;i++)
-        {
-            dp[i][0]=1;
-        }
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(s.charAt(i-1)==t.charAt(j-1)){
-                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
-                }else{
-                    dp[i][j]=dp[i-1][j];
-                }
-                
-            }  
-        }
-        return dp[m][n];
+    public static int helper(String s, String t, int i, int j, int dp[][]) {
+    if(j == 0) {
+        return 1;
     }
+    if(i == 0) return 0;
+    if(dp[i][j] != -1) {
+        return dp[i][j];
+    }
+    if(s.charAt(i-1) == t.charAt(j-1)) {
+        dp[i][j] = helper(s, t, i-1, j-1, dp) + helper(s, t, i-1, j, dp);
+        return dp[i][j];
+    } else {
+        return dp[i][j] = helper(s, t, i-1, j, dp);
+    }
+}
+
+public static int numDistinct(String s, String t) {
+    int m = s.length();
+    int n = t.length();
+    int dp[][] = new int[m+1][n+1];
+    for(int i = 0; i <= m; i++) {
+        dp[i][0] = 1;
+    }
+    for(int i = 1; i <= m; i++) {
+        for(int j = 1; j <= n; j++) {
+            dp[i][j] = -1;
+        }
+    }
+    return helper(s, t, m, n, dp);
+}
+
 }
