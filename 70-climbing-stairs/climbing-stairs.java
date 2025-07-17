be0a4a860e6a07ca848e049b33040874876a2e0f
Gourab Choudhury <129882446+gourab9817@users.noreply.github.com>
2025-07-18 02:08:44 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (92.20%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -7,11 +7,30 @@ class Solution {
         if(n==1||n==2||n==0){
             return n;
         }else{
-        
+                                            //Tabulation
         for(int i=3;i<=n;i++){
             dp[i]=dp[i-1]+dp[i-2];
         }
         }
         return dp[n];
     }
+
+    // public int climbStairs(int n){
+    //     if(n==0||n==1||n==2){
+    //         return n;
+
+    //     }
+    //     return climbStairs(n-1)+climbStairs(n-2);
+    // }
+
+// public int climbStairs(int n,int dp[]){
+//     dp[0]=0;
+//     dp[1]=1;
+//     dp[2]=2;
+//     if(dp[n]!=0){
+//         return dp[n];               // ,e,orization
+//     }
+//     dp[n]=climbStairs(n-1,dp)+climbStairs(n-2,dp);
+//     return dp[n];
+// }
 }
