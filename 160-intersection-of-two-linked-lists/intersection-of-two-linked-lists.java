82b2e9b794297dab1755c4c0a14a15e76d8b2dee
Gourab Choudhury <129882446+gourab9817@users.noreply.github.com>
2025-08-12 17:21:34 +0530

    Time: 278 ms (8.34%) | Memory: 48 MB (88.06%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -10,28 +10,50 @@
  * }
  */
 public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    // public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    //     ListNode temp1=headA;
+    //     ListNode temp2=headB;
+    //     ArrayList<ListNode> al1=new ArrayList<>();
+    //     while(temp1!=null){
+    //         al1.add(temp1);
+    //         temp1=temp1.next;
+    //     }
+    //     ArrayList<ListNode> al2=new ArrayList<>();
+    //     while(temp2!=null){
+    //         al2.add(temp2);
+    //         temp2=temp2.next;
+    //     }
+    //     ListNode x=null;
+    //     for(int i=0;i<al1.size();i++){
+    //         for(int j=0;j<al2.size();j++){
+    //             if(al1.get(i).equals(al2.get(j))){
+    //                 x=al1.get(i);
+    //                 return x;
+    //             }
+    //         }
+    //     }
+    //     return null;
+    // }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB){
         ListNode temp1=headA;
         ListNode temp2=headB;
-        ArrayList<ListNode> al1=new ArrayList<>();
-        while(temp1!=null){
-            al1.add(temp1);
-            temp1=temp1.next;
-        }
-        ArrayList<ListNode> al2=new ArrayList<>();
-        while(temp2!=null){
-            al2.add(temp2);
-            temp2=temp2.next;
-        }
-        ListNode x=null;
-        for(int i=0;i<al1.size();i++){
-            for(int j=0;j<al2.size();j++){
-                if(al1.get(i).equals(al2.get(j))){
-                    x=al1.get(i);
-                    return x;
-                }
+         if (headA == null || headB == null) return null;
+
+
+        while(temp1!=temp2){
+            if(temp1==null){
+                temp1=headA;
+            }else{
+                temp1=temp1.next;
+            }
+
+            if(temp2==null){
+                temp2=headB;
+            }else{
+                temp2=temp2.next;
             }
         }
-        return null;
+        return temp1;
     }
 }
