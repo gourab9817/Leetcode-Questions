8033182b0a4596635e1dc300529d0bd207b6b27e
Gourab Choudhury <129882446+gourab9817@users.noreply.github.com>
2025-09-15 14:54:17 +0530

    Time: 2 ms (91.65%) | Memory: 42.1 MB (82.37%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
@@ -1,18 +1,25 @@
 class Solution {
     public int canBeTypedWords(String text, String b) {
-    char arr[] = b.toCharArray();
-    String words[] = text.split(" ");
+    String[] words = text.split(" ");
     int count = 0;
 
-    outer: for (String w : words) {
-        for (int j = 0; j < arr.length; j++) {
-            if (w.indexOf(arr[j]) != -1) { // if word contains broken letter
-                continue outer;           // skip this word
+    for (int i = 0; i < words.length; i++) {
+        String w = words[i];
+        boolean ok = true;
+
+        for (int j = 0; j < b.length(); j++) {
+            char c = b.charAt(j);
+            if (w.indexOf(c) != -1) { // word has a broken letter
+                ok = false;
+                break;
             }
         }
-        count++; 
+
+        if (ok) count++;
     }
+
     return count;
 }
+
 
 }
