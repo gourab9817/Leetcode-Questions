44142915cac26ac8bbcf355f5a80e3500dbe80d3
Gourab Choudhury <129882446+gourab9817@users.noreply.github.com>
2026-05-07 23:35:19 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (31.50%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,42 +1,22 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int n=nums.length;
-        // return search3(nums,target,0,n-1);
-        return search2(nums,target);
-    }
     
-    public int search2(int[] nums, int target) {
-        int n=nums.length;
+    public  int search(int[] nums, int target){
         int start=0;
         int end=nums.length-1;
+        int mid=(start+end)/2;
         while(start<=end){
-            int mid=(start+end)/2;
-            if (nums[mid] == target) return mid;
-
-            else if(nums[mid]<target){
-                start=mid+1;
-                
+            if(nums[mid]==target){
+                return mid;
             }
-            else{
+            else if(target<nums[mid]){
                 end=mid-1;
+                mid=(start+end)/2;
+            }else{
+                start=mid+1;
+                mid=(start+end)/2;
             }
+            
         }
         return -1;
-    }
-    public int search3(int[] nums, int target,int start,int end) {
-        int n=nums.length;
-        int mid=(start+end)/2;
-        if (start > end) return -1;
-        if(nums[mid]==target){
-            return target;
-        }
-        if(nums[mid]>target){
-            end=mid-1;
-            return search3(nums,target,start,end);
-        }else {
-            start=mid+1;
-            return search3(nums,target,start,end);
-        }
-        
     }
 }
