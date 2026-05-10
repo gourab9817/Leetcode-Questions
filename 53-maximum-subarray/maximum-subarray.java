36af958d0affb0caa26e76d3ba1f38a994c0884b
Gourab Choudhury <129882446+gourab9817@users.noreply.github.com>
2026-05-10 18:34:45 +0530

    Time: 1 ms (99.97%) | Memory: 76.9 MB (91.30%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,25 +1,33 @@
 class Solution {
-    public int maxSubArray(int[] nums) {
+    // public int maxSubArray(int[] nums) {
+    //     int arr[]=new int[nums.length];
+    //     arr[0]=nums[0];
+    //     for(int i=1;i<nums.length;i++){
+    //         arr[i]=arr[i-1]+nums[i];
+    //     }
+    //     int mx=Integer.MIN_VALUE;
+    //     for (int i=0;i<nums.length;i++){
+
+    //         int start=i;
+    //         for(int j=i;j<nums.length;j++){
+    //             int end=j;
+
+    //             int prefix= start==0 ? arr[end]:arr[end]-arr[start-1];
+    //             mx=Math.max(mx,prefix);
+    //         }
+    //     }
+    //     return mx;
+    // }
+
+    public int maxSubArray(int[] nums){
         int curr=0;
-        int max=Integer.MIN_VALUE;
-        int min=Integer.MIN_VALUE;
-        int x=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]<0){
-                min=Math.max(min,nums[i]);
-                x++;
-            }
-            if(x==nums.length){
-                return min;
-            }
-        }
+        int mx=Integer.MIN_VALUE;
         for(int i=0;i<nums.length;i++){
             curr+=nums[i];
-            if(curr<0){
-                curr=0;
-            }
-            max=Math.max(curr,max);
+            mx=Math.max(curr,mx);
+            curr = curr<=0 ? 0:curr;
+            
         }
-        return max;
+        return mx;
     }
 }
