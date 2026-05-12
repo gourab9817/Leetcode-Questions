a61b95bb16965e58f3bf3404e08cda0a0d9371d9
Gourab Choudhury <129882446+gourab9817@users.noreply.github.com>
2026-05-12 23:37:45 +0530

    Time: 5 ms (82.72%) | Memory: 77.5 MB (12.14%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,27 +1,17 @@
-public class Solution {
-    public static int maxArea(int[] containerval) {
-        int maxwater = 0;
-        int Lp = 0;
-        int rp = containerval.length - 1;
-        
-        while (Lp < rp) {
-            int width = rp - Lp;
-            int height = Math.min(containerval[Lp], containerval[rp]);
-            int water = width * height;
-            if (maxwater < water) {
-                maxwater = water;
+class Solution {
+    public int maxArea(int[] height) {
+        int i=0,j=height.length-1;
+        int left=0,right=0,max=0;
+        while(i<j){
+            left=height[i];
+            right=height[j];
+            if(left<right){
+                i++;
+            }else{
+                j--;
             }
-
-            // Move the pointer that points to the shorter line
-            if (containerval[Lp] < containerval[rp]) {
-                Lp++;
-            } else {
-                rp--;
-            }
+            max=Math.max(max,(Math.min(left,right)*(j-i+1)));
         }
-        
-        return maxwater; 
+        return max;
     }
-
-    
 }
